190c135b1cc575ab34443f0b85461a944f62c750
Phil DeLong <philjdelong@gmail.com>
2020-01-28 12:47:56 -0700

    Create merchant serializer

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::MerchantsController < ApplicationController
 
   def index
-    render json: Merchant.all
+    merchants = Merchant.all
+    render json: MerchantSerializer.new(merchants)
   end
 
   def show
-    render json: Merchant.find(params[:id])
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
   end
 end
