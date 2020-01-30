f3771c2cda19fd51231159b059c826cca1981684
Phil DeLong <philjdelong@gmail.com>
2020-01-30 12:55:18 -0700

    Include route for customer/find

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get '/customers/find', to: "find#show"
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index]
         resources :favorite_customer, only: [:index]
