a8833274b1d731cc9bf3b3e51df08e940285620a
Phil DeLong <philjdelong@gmail.com>
2020-01-30 15:27:17 -0700

    Include customer transactions route

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
       resources :items, only: [:index, :show]
       resources :customers, only: [:index, :show] do
         resources :invoices, only: [:index]
+        resources :transactions, only: [:index]
       end
       resources :invoice_items, only: [:index, :show]
     end
