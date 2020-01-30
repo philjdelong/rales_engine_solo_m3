2e9487e55fba4905a715a8513c3fb25836e27a65
Phil DeLong <philjdelong@gmail.com>
2020-01-30 16:09:20 -0700

    Include conditional for invoice

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,6 +4,9 @@ class Api::V1::ItemsController < ApplicationController
     if params[:merchant_id]
       merchant = Merchant.find(params[:merchant_id])
       items = merchant.items
+    elsif params[:invoice_id]
+      invoice = Invoice.find(params[:invoice_id])
+      items = invoice.items
     else
       items = Item.all
     end
--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::TransactionsController < ApplicationController
 
   def index
-    customer = Customer.find(params[:customer_id])
-    transactions = customer.transactions
+    if params[:customer_id]
+      customer = Customer.find(params[:customer_id])
+      transactions = customer.transactions
+    elsif params[:invoice_id]
+      invoice = Invoice.find(params[:invoice_id])
+      transactions = invoice.transactions
+    end
     render json: TransactionSerializer.new(transactions)
   end
 end
