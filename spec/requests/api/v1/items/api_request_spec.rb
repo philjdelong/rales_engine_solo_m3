1d6aecbcf60a482ea3c84e8256e974ada4a7e386
Phil DeLong <philjdelong@gmail.com>
2020-01-30 20:57:38 -0700

    Add test for merchant/ invoice items

--- spec/requests/api/v1/items/api_request_spec.rb
+++ spec/requests/api/v1/items/api_request_spec.rb
@@ -1,26 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe "Items API" do
-  it "sends a list of items" do
-    merchant = create(:merchant)
-    create_list(:item, 12, merchant_id: merchant.id)
+  describe "endpoints" do
+    it "sends a list of items" do
+      merchant = create(:merchant)
+      create_list(:item, 12, merchant_id: merchant.id)
 
-    get "/api/v1/items"
-    expect(response).to be_successful
+      get "/api/v1/items"
+      expect(response).to be_successful
 
-    items = JSON.parse(response.body)
+      items = JSON.parse(response.body)
 
-    expect(items["data"].count).to eq(12)
+      expect(items["data"].count).to eq(12)
+    end
+
+    it "sends a single item" do
+      merchant = create(:merchant)
+      item = create(:item, merchant_id: merchant.id)
+
+      get "/api/v1/items/#{item.id}"
+      expect(response).to be_successful
+
+      item_info = JSON.parse(response.body)
+      expect(item_info["data"]["attributes"]["id"]).to eq(item.id)
+    end
   end
 
-  it "sends a single item" do
-    merchant = create(:merchant)
-    item = create(:item, merchant_id: merchant.id)
+  describe "relationships" do
+    it "sends the merchant" do
+      merchant = create(:merchant, id: 1)
+      item = create(:item, merchant_id: merchant.id)
 
-    get "/api/v1/items/#{item.id}"
-    expect(response).to be_successful
+      get "/api/v1/items/#{item.id}/merchant"
+      expect(response).to be_successful
 
-    item_info = JSON.parse(response.body)
-    expect(item_info["data"]["attributes"]["id"]).to eq(item.id)
+      merchant_info = JSON.parse(response.body)["data"]
+      expect(merchant_info["attributes"]["id"]).to eq(merchant.id)
+    end
+
+    it "sends a list of invoice_items" do
+      merchant = create(:merchant)
+      customer = create(:customer)
+      item = create(:item, merchant_id: merchant.id)
+      invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+      create_list(:invoice_item, 10, invoice_id: invoice.id, item_id: item.id)
+
+      get "/api/v1/items/#{item.id}/invoice_items"
+      expect(response).to be_successful
+
+      invoice_items_info = JSON.parse(response.body)['data']
+      expect(invoice_items_info.count).to eq(10)
+    end
   end
 end
