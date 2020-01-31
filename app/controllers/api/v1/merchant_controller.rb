60c8073f5e0e764280b8533b60e4bc54d9faaa11
Phil DeLong <philjdelong@gmail.com>
2020-01-30 20:57:58 -0700

    Update controllers

--- app/controllers/api/v1/merchant_controller.rb
+++ app/controllers/api/v1/merchant_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::MerchantController < ApplicationController
 
   def index
-    invoice = Invoice.find(params[:invoice_id])
-    merchant = invoice.merchant
+    if params[:invoice_id]
+      invoice = Invoice.find(params[:invoice_id])
+      merchant = invoice.merchant
+    elsif params[:item_id]
+      item = Item.find(params[:item_id])
+      merchant = item.merchant
+    end
     render json: MerchantSerializer.new(merchant)
   end
 end
