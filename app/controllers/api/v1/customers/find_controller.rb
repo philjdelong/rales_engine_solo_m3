ef9151ef2379adf8c484c2a1bb2e79daf713bd3c
Phil DeLong <philjdelong@gmail.com>
2020-02-01 11:10:24 -0700

    Create find and find_all methods for find controller

--- app/controllers/api/v1/customers/find_controller.rb
+++ app/controllers/api/v1/customers/find_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::Customers::FindController < ApplicationController
     customer = Customer.find_by(request.query_parameters)
     render json: CustomerSerializer.new(customer)
   end
+
+  def index
+    customers = Customer.where(request.query_parameters)
+    render json: CustomerSerializer.new(customers)
+  end
 end
