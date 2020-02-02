bddde8a4e3c71110e9b563da5fb8c9ab0781c8ac
Phil DeLong <philjdelong@gmail.com>
2020-02-01 19:58:20 -0700

    Update controller and model to include favorite merchant

--- app/controllers/api/v1/customers/find_controller.rb
+++ app/controllers/api/v1/customers/find_controller.rb
@@ -1,12 +1,17 @@
 class Api::V1::Customers::FindController < ApplicationController
 
   def show
-    customer = Customer.find_by(request.query_parameters)
+    customer = Customer.find_by(customer_info)
     render json: CustomerSerializer.new(customer)
   end
 
   def index
-    customers = Customer.where(request.query_parameters)
+    customers = Customer.where(customer_info)
     render json: CustomerSerializer.new(customers)
   end
+
+  private
+    def customer_info
+      params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
+    end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,15 @@ class Customer < ApplicationRecord
 
   has_many :invoices
   has_many :transactions, through: :invoices
+
+  def favorite_merchant
+    Merchant.
+      select('merchants.*, COUNT(transactions.id) AS total_successful_transactions').
+      joins(invoices: :transactions).
+      merge(Transaction.successful).
+      where("#{self.id} = invoices.customer_id").
+      group('merchants.id').
+      order('total_successful_transactions DESC').
+      limit(1).first
+  end
 end
